f530ee0639dd13ba389d5ac56de4343fb5e17cfd
drissnafii <drissnafi3@gmail.com>
2026-03-04 09:22:13 +0000

    fix: resolve typescript error in package details screen by using valid color key

--- app/package/[id].tsx
+++ app/package/[id].tsx
@@ -86,7 +86,7 @@ const styles = StyleSheet.create({
   },
   emptyText: {
     fontSize: 16,
-    color: Colors.neutral[400],
+    color: Colors.neutral[500],
   },
   footer: {
     flexDirection: "row",
